e75e612da0e02b8e8e6e84f6f37ac99d23637ce3
Jere Viitasalo <jere.viitasalo@gmail.com>
2024-11-21 22:04:41 +0200

    Added Course module for exercise 2.5

--- Osa2/kurssitiedot/src/App.jsx
+++ Osa2/kurssitiedot/src/App.jsx
@@ -1,48 +1,4 @@
-const Header = ({course}) => {
-  return (
-    <div>
-      <h1>{course.name}</h1>
-    </div>
-  )
-}
-
-const Part = ({name, exercises}) => {
-  return (
-    <div>
-      {name} {exercises}
-    </div>
-  )
-}
-
-const Content = ({parts}) => {
-  return (
-    parts.map(part => (
-      <div key={part.id}>
-        <Part name={part.name} exercises={part.exercises}/>
-      </div>
-    ))
-  )
-}
-
-const Total = ({course}) => {
-  const sum = course.parts.reduce((total, part) => total + part.exercises, 0)
-
-  return (
-    <div>
-      Total of {sum} exercises
-    </div>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header course={course}/>
-      <Content parts={course.parts}/>
-      <Total  course={course}/>
-    </div>
-  )
-}
+import Course from './components/Course.jsx'
 
 const App = () => {
   const courses = [
