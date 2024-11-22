3c10610994acaffb5f8d4ede2ce6e6163e671c32
Jere Viitasalo <jere.viitasalo@gmail.com>
2024-11-23 01:11:19 +0200

    Added puhelinluettelo step 1 and 2

---
+++ Osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName
+    }
+    if (persons.some(user => user.name === nameObject.name))
+      alert(`${nameObject.name} is already added to phonebook`)
+    else  setPersons(persons.concat(nameObject))
+    setNewName("")
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(element => <div key={element.name}>{element.name}</div>)}
+    </div>
+  )
+
+}
+
+export default App
